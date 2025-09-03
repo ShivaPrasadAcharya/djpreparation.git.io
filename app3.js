1453a1ab033e9e9750d7520e6de4ae6005c6284e
ShivaPrasadAcharya <shivaprasadacharya000000000000@gmail.com>
2025-09-03 16:55:56 +0000

    compatible banayeko

--- app3.js
+++ app3.js
@@ -96,7 +96,7 @@ DataApp.prototype.getHTML = function() {
     return `
         <div class="container">
             <div class="header">
-                <h2>🗃️Sample of Management System(SMS)</h2>
+                <h2>🗃️DJ Prep Mgmt System(DJPMS)</h2>
                 <p>Developed by: शिवप्रसाद आचार्य (२०८२, श्रीकृष्णजन्माष्टमी) </p>
             </div>
 
