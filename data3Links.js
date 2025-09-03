ce89e8aa574f82a0fa0957510300d0f2fa7f7305
ShivaPrasadAcharya <shivaprasadacharya000000000000@gmail.com>
2025-09-03 22:19:15 +0545

    Update data3Links.js

--- data3Links.js
+++ data3Links.js
@@ -1,5 +1,21 @@
 // data3Links.js - Links Dataset
 var data3Links = `ID,Categry,Subject,Remark,Link
+011,Nyayik Awaj,Content/topic,pdf,DOCS/nyayikawajcontent.pdf
+01,dj ppt, civillaw, pdf, DOCS/B A 1 civil law.pdf
+02,dj ppt, strategic plan, pdf, DOCS/A E 2 Strategic Plan.pdf
+03,dj ppt, Nepalese legal system, pdf, DOCS/A C 1 Nepalese_Legal_System for judges.pdf
+04,dj ppt, देवानी कानूनको अवधारणा,pdf, DOCS/B A 1 civillaw.pdf
+05,dj ppt, succession, pdf,DOCS/B B 1 f succession.pdf
+06,dj ppt, guardian, pdf,DOCS/B B 1 g guardian.pdf
+07,dj ppt, adoption, pdf,DOCS/B B 1 j adoption.pdf
+08,dj ppt, करार, pdf,DOCS/B B 2 contract.pdf
+09,dj ppt, Dwarikaman Joshi-dewani-karyabidhi, pdf, DOCS/B D 1 Dwarikaman Joshi  p.p-dewani-karyabidi-kanun.pdf
+10,dj ppt, Dwarikaman Joshi-dewani-karyabidhi, pdf, DOCS/B D 1-4 Dwarikaman Joshi  p.p-dewani-karyabidi-kanun.pdf
+11,dj ppt, General Principles of law and judicial response, pdf, DOCS/C A 1 General Principles of law and judicial response.pdf
+12,dj ppt, Victim Justice System, pdf, DOCS/C A 4 VictimJusticeSystem.pdf
+13,dj ppt, property offence, pdf, DOCS/C B 4 h property offence.pdf
+14,dj ppt, Organized Crime, pdf, DOCS/C B 5 Organized Crime20801107.pdf
+15,dj ppt, Reformative Justice System, pdf, DOCS/C E 1 Reformative Justice System Upreti.pdf
 
 `;
 // Dataset metadata
@@ -10,5 +26,6 @@ var data3LinksInfo = {
     columns: 5,
     primaryKey: "ID"
 };
+
 
 
