3bce351f0b28ff14b6fc982ee06e9bf4bd72c01e
ShivaPrasadAcharya <shivaprasadacharya000000000000@gmail.com>
2025-09-24 06:17:27 +0000

    ok

--- data3Links.js
+++ data3Links.js
@@ -1,8 +1,9 @@
 // data3Links.js - Links Dataset
 var data3Links = `ID,Categry,Subject,Remark,Link
 100,syllables,DJ syllables,pdf,DOCS/districtjudgesyllables.pdf
-101,Handwritten Notes1,DJ Notes1,pdf,DOCS/DJWrittenNotes1.pdf
-102,Handwritten Notes2,DJ Notes2,pdf,DOCS/DJWrittenNotes2.pdf
+101,Handwritten Notes1,Random Notes1,pdf,DOCS/DJWrittenNotes1.pdf
+102,Handwritten Notes2,Random Notes2,pdf,DOCS/DJWrittenNotes2.pdf
+103,Handwritten Notes3,Random Notes3,pdf,DOCS/DJWrittenNotes3.pdf
 200,Nyayik Awaj,Content/topic,pdf,DOCS/nyayikawajcontent.pdf
 01,dj ppt, civillaw, pdf, DOCS/B A 1 civil law.pdf
 02,dj ppt, strategic plan, pdf, DOCS/A E 2 Strategic Plan.pdf
